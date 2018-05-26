5b32fa2c4d7698195fa5b8a9c33ffdf777a201d0
ROUSHAN <36974697+RKRroushan@users.noreply.github.com>
2018-05-26 10:28:54 +0530

    Update connection.js
    I am here remove some code which contains before function which is not necessary for connection.So,thats why i removed it.

--- connection.js
+++ connection.js
@@ -1,26 +1,13 @@
 const mongoose = require('mongoose');
 
 // ES6 Promises
-mongoose.Promise = global.Promise;
-
-// Connect to db before tests run
-before(function(done){
+mongoose.Promise = global.Promise;//(optinal)this line you have to write only when your programm needs Promise
 
     // Connect to mongodb
     mongoose.connect('mongodb://localhost/testaroo');
     mongoose.connection.once('open', function(){
         console.log('Connection has been made, now make fireworks...');
-        done();
     }).on('error', function(error){
         console.log('Connection error:', error);
     });
 
-});
-
-// Drop the characters collection before each test
-beforeEach(function(done){
-    // Drop the collection
-    mongoose.connection.collections.mariochars.drop(function(){
-        done();
-    });
-});
